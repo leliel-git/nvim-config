d9ac5f43d14a3a0567d4cb51b03dbbb3ab3f77b9
leliel <mail.leliel@proton.me>
2024-08-12 22:44:27 +0000

    added readme

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function () 
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "rust", "java", "markdown" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
+		configs.setup({
+			ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "rust", "java", "markdown" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end
  }
