07c2599b5ed1f8f5e1de85e7b83e9c526034fd6d
leliel <mail.leliel@proton.me>
2024-08-26 03:56:53 +0000

    added vim theme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,5 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.cmd [[ colorscheme vim ]]
 -- Makes rust source files respect users choice to use tabs for what they're meant to
 vim.g.rust_recommended_style = false
