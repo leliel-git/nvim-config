a0a1eb3cb9c8280fb786d11a5fbcbf5893250e40
leliel <mail.leliel@proton.me>
2024-08-26 05:23:18 +0000

    Added transparency in the background to nvimgelion

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,4 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.cmd [[ colorscheme vim ]]
 -- Makes rust source files respect users choice to use tabs for what they're meant to
 vim.g.rust_recommended_style = false
