6306146a995ac6ea9be24b5c98ad9c67d0f71cbb
leliel <mail.leliel@proton.me>
2025-04-04 00:39:36 +0000

    added 2 space identation for html and xml files

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,3 +8,12 @@ vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 -- Makes rust source files respect users choice to use tabs for what they're meant to
 vim.g.rust_recommended_style = false
+-- 2 space indentation for .xml and .html files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {"html", "xml"},
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.expandtab = true
+	end,
+})
